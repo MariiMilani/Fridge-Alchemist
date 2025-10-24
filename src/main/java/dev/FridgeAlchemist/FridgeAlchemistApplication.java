6ccc3a995b5fa80705f94d073398e1541db9f226
Mariana Milani <mari.m.assuncao@gmail.com>
2025-10-24 10:38:00 -0300

    first commit

---
+++ src/main/java/dev/FridgeAlchemist/FridgeAlchemistApplication.java
@@ -0,0 +1,13 @@
+package dev.FridgeAlchemist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeAlchemistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeAlchemistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/FridgeAlchemist/FridgeAlchemistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.FridgeAlchemist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeAlchemistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
